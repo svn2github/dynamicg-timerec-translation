752c6105ae0ca5b78eb3074150e47dcf353d27d7
dynamicg.android@gmail.com <dynamicg.android@gmail.com@8a2e902c-8577-b19d-e130-db3a23dfb8c2>
2011-03-29 15:22:50 +0000

    "R.string.stdCommentDefaultEntryX" for hard coded texts
    git-svn-id: http://dynamicg-timerec.googlecode.com/svn/trunk@26 8a2e902c-8577-b19d-e130-db3a23dfb8c2

---
+++ timerec.translations/copyEnDe.bat
@@ -0,0 +1,5 @@
+cd /d C:\workspace\android
+copy TimeRecording\res\values\strings.xml timerec.translations\res\values\.
+copy TimeRecording\res\values-de\strings.xml timerec.translations\res\values-de\.
+
+pause
