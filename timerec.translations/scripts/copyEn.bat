caa6847ed80f18a6ca7a7a730090c5481b2543ea
dynamicg.android@gmail.com <dynamicg.android@gmail.com@8a2e902c-8577-b19d-e130-db3a23dfb8c2>
2012-04-30 09:32:09 +0000

    git-svn-id: http://dynamicg-timerec.googlecode.com/svn/trunk@334 8a2e902c-8577-b19d-e130-db3a23dfb8c2

--- timerec.translations/scripts/copyEn.bat
+++ timerec.translations/scripts/copyEn.bat
@@ -1,6 +1,5 @@
 cd /d %0\..\..\..
 
 copy TimeRecording\res\values\strings.xml timerec.translations\res\values\.
-copy TimeRecording\res\values-de\strings.xml timerec.translations\res\values-de\.
 
 pause
