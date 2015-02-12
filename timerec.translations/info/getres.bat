0ac680dfe5fdfafc6f0e2f82ec529d69560371e7
dynamicg.android@gmail.com <dynamicg.android@gmail.com@8a2e902c-8577-b19d-e130-db3a23dfb8c2>
2015-02-12 09:58:42 +0000

    git-svn-id: http://dynamicg-timerec.googlecode.com/svn/trunk@1219 8a2e902c-8577-b19d-e130-db3a23dfb8c2

--- timerec.translations/info/getres.bat
+++
@@ -1,5 +0,0 @@
-SET SRC=C:\workspace\androidTR\TimeRecording\res
-SET TARGET=C:\workspace\androidTR\timerec.translations\res
-
-copy %SRC%\values\strings.xml %TARGET%\values-en\.
-copy %SRC%\values-de\strings.xml %TARGET%\values-de\.
